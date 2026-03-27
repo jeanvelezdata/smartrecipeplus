ead8e6eb6da3c8b18454bb80b40bfd89aab8fa94
Jean Velez <jean.velez5@upr.edu>
2026-03-27 17:47:08 -0400

    fix: int casting error in evaluate.py

--- unsupervised/evaluate.py
+++ unsupervised/evaluate.py
@@ -45,7 +45,7 @@ class Food101LabeledDataset(Dataset):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        item = self.dataset[idx]
+        item = self.dataset[int(idx)]
         image = item["image"].convert("RGB")
         label = item["label"]
         if self.transform is not None:
